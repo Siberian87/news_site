0df63190febc87b82ab2ed28027cb9ed273883ef
Viktor Bolotnov <siberiancoldman87@gmail.com>
2020-12-09 06:28:23 -0800

    Переписан проект с использованием blueprint. Добавлено меню и декоратор @admin_required.

--- webapp/user/models.py
+++ webapp/user/models.py
@@ -1,20 +1,7 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
 
-
-db = SQLAlchemy()
-
-class News(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    url = db.Column(db.String, unique=True, nullable=False)
-    published = db.Column(db.DateTime, nullable=False)
-    text = db.Column(db.Text, nullable=True)
-
-    def __repr__(self):
-        return f'<News {self.title} {self.url}>'
-
+from webapp.db import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,5 +21,3 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return '<User name={} id={}>'.format(self.username, self.id)
-
-        
